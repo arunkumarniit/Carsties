c98445c4b2be12b9f614ba4bcfc100b0c4cb821c
arunkumarniit <arunkumar.niit@gmail.com>
2024-04-20 06:41:13 +0530

    End of section 6

---
+++ src/AuctionService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,31 @@
+using AuctionService.Data;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService.Consumers;
+
+public class BidPlacedConsumer : IConsumer<BidPlaced>
+{
+    private readonly AuctionDbContext dbContext;
+
+
+    public BidPlacedConsumer(AuctionDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+
+    }
+    public async Task Consume(ConsumeContext<BidPlaced> context)
+    {
+        System.Console.WriteLine("--> Consuming Bid Placed");
+         var auction = await dbContext.Auctions.FindAsync(context.Message.AuctionId);
+
+         if(auction.CurrentHighBid == null 
+            || context.Message.BidStatus.Contains("Accepted")
+            && context.Message.Amount > auction.CurrentHighBid)
+         {
+            auction.CurrentHighBid = context.Message.Amount;
+            await dbContext.SaveChangesAsync();
+         }
+    }
+
+}
